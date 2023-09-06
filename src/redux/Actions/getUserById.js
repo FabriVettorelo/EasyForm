e8e4b47136e0dd8cdaf9eee48ea74278c9a9ee55
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-09-06 14:58:26 -0300

    Update getUserById.js

--- src/redux/Actions/getUserById.js
+++ src/redux/Actions/getUserById.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-
 export const GET_USER_ID = "GET_USER_ID";
 
 export const getUserById = (id) => {
   return async (dispatch) => {
-    let info = await axios.get(`http://localhost:3001/users/${id}`);
-    //let info = await axios.get(`https://sportiverse-server.onrender.com/activities/${id}`);
+    //let info = await axios.get(`http://localhost:3001/users/${id}`);
+    let info = await axios.get(`https://formserver-t5jb.onrender.com/users/${id}`);
     return dispatch({ type: GET_USER_ID, payload: info.data });
   };
 };
