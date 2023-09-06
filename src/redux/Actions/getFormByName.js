cbf9ab25cc7393c92ed24c6849ea5e33743cf528
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-09-06 14:51:24 -0300

    Update getFormByName.js

--- src/redux/Actions/getFormByName.js
+++ src/redux/Actions/getFormByName.js
@@ -6,37 +6,26 @@ export const GET_FORM_NAME = "GET_FORM_NAME";
 export default function getFormByName(name) {
     return async function (dispatch) {
         try {
-            let result = await axios.get(
-                  `http://localhost:3001/forms?name=${name}`
-            );
-
+            let result = await axios.get(`https://formserver-t5jb.onrender.com/forms?name=${name}`);
             if (result.data.length === 0) {
                 Swal.fire({
                     icon: 'error',
-                    title: 'Búsqueda sin resultados',
-                    text: 'No se encontraron resultados para el formulario consultado ',
+                    title: 'Sin resultados',
+                    text: 'No se encontraron resultados',
                     showConfirmButton: false, 
                     timer: 3000, 
                     background: '#FFFFFF',
                     timerProgressBar: true,
                     backdrop: true,
-                    showClass: {
-                        popup: 'animate__animated animate__fadeInDown'
-                    },
-                    hideClass: {
-                        popup: 'animate__animated animate__fadeOutUp'
-                    }
-                    
-                    
+                    showClass: { popup: 'animate__animated animate__fadeInDown'},
+                    hideClass: { popup: 'animate__animated animate__fadeOutUp'}                    
                 });
-    
             } else {
                 return dispatch({
                     type: GET_FORM_NAME,
                     payload: result.data,
                 });
             }
-
         } catch (error) {
             console.log("Error in Action GET_FORM_NAME: ", error);
         }
