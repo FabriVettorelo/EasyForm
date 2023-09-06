13768974fa503ad0bf69f77c5e6d969c4d2fa623
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-09-06 14:56:53 -0300

    Update updateResponse.js

--- src/redux/Actions/updateResponse.js
+++ src/redux/Actions/updateResponse.js
@@ -1,14 +1,9 @@
 import axios from 'axios'
-
 export const UPDATE_RES = "UPDATE_RES";
 
 export const updateResponse = (id, formValues) => {
-    return async (dispatch) => {
-    
-        const info = await axios.put(`http://localhost:3001/response/${id}`, formValues);
-        console.log(formValues);
-        console.log(id);
+    return async (dispatch) => {    
+        const info = await axios.put(`https://formserver-t5jb.onrender.com/response/${id}`, formValues);
         return dispatch({ type: UPDATE_RES, payload: info.data });
-
     }
 }
