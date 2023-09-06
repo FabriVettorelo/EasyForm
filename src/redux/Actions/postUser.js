a3be736c72e6947e09b5e48504478a3405de9542
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-09-06 15:00:21 -0300

    Update postUser.js

--- src/redux/Actions/postUser.js
+++ src/redux/Actions/postUser.js
@@ -1,12 +1,10 @@
 import axios from "axios";
-
 export const POST_USER = 'POST_USER';
 
 export const postUser = (user) => {
     return async function () {
       const response = await axios.post(
-          "http://localhost:3001/users", user);
-    
+          "https://formserver-t5jb.onrender.com/users", user);
       return response;
     };
 };
