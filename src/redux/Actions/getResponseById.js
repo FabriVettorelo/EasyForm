267b2e6ccdac2ea18d4cc20ffffeb2160a2c4c5c
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-09-06 14:54:03 -0300

    Update getResponseById.js

--- src/redux/Actions/getResponseById.js
+++ src/redux/Actions/getResponseById.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-
 export const GET_RES_ID = "GET_RES_ID";
 
 export const getResponseById = (id) => {
   return async (dispatch) => {
-    let info = await axios.get(`http://localhost:3001/response/${id}`);
+   //let info = await axios.get(`http://localhost:3001/response/${id}`);
+    let info = await axios.get(`https://formserver-t5jb.onrender.com/response/${id}`);
     return dispatch({ type: GET_RES_ID, payload: info.data });
   };
 };
