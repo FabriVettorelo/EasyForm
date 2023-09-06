1868ac843c4b5b31d7828aba8df14c93a793549a
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-09-06 14:55:39 -0300

    Update postResponses.js

--- src/redux/Actions/postResponses.js
+++ src/redux/Actions/postResponses.js
@@ -1,12 +1,10 @@
 import axios from 'axios'
-
 export const POST_RES = "POST_RES";
 
 export const postResponses = (payload) => {
     return async (dispatch) => {
 
-         let info = await axios.post('http://localhost:3001/response',payload);
-
+         let info = await axios.post('https://formserver-t5jb.onrender.com/response',payload);
         return dispatch({ type: POST_RES, payload: info.data });
     }
 }
