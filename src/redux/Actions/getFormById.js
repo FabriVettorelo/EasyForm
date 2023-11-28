cb61c47d065ba2e1aee954a7c3f06ab70ff09703
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-11-28 17:52:42 -0300

    Update getFormById.js

--- src/redux/Actions/getFormById.js
+++ src/redux/Actions/getFormById.js
@@ -5,7 +5,9 @@ export const GET_FORM_ID = "GET_FORM_ID";
 export const getFormById = (id) => {
   return async (dispatch) => {
     //let info = await axios.get(`http://localhost:3001/forms/${id}`);
-    let info = await axios.get(`https://formserver-t5jb.onrender.com/forms/${id}`);
+    //let info = await axios.get(`https://formserver-t5jb.onrender.com/forms/${id}`);
+     let info = await axios.get(`https://easyform-server-production.up.railway.app/forms/${id}`);
+    
     return dispatch({ type: GET_FORM_ID, payload: info.data });
   };
 };
