e943d626743d8503c81f07d9bdc6b242f78398b2
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-11-28 17:57:09 -0300

    Update postResponses.js

--- src/redux/Actions/postResponses.js
+++ src/redux/Actions/postResponses.js
@@ -4,7 +4,9 @@ export const POST_RES = "POST_RES";
 export const postResponses = (payload) => {
     return async (dispatch) => {
 
-         let info = await axios.post('https://formserver-t5jb.onrender.com/response',payload);
+       //  let info = await axios.post('https://formserver-t5jb.onrender.com/response',payload);
+        let info = await axios.post('https://easyform-server-production.up.railway.app/response',payload);
+        
         return dispatch({ type: POST_RES, payload: info.data });
     }
 }
