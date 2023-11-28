af76c236b7a30646865f124eec73d9cdaae071f7
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-11-28 17:56:32 -0300

    Update login.js

--- src/redux/Actions/login.js
+++ src/redux/Actions/login.js
@@ -4,7 +4,9 @@ export const LOGIN = 'LOGIN';
 export const login = (user) => {
     return async function (dispatch) {
       try {
-      const response = await axios.post("https://formserver-t5jb.onrender.com/login", user);
+      //const response = await axios.post("https://formserver-t5jb.onrender.com/login", user);
+          const response = await axios.post("https://easyform-server-production.up.railway.app/login", user);
+          
         return  dispatch({type: LOGIN, payload: response.data});
       } catch (error) {
         throw error; }     
