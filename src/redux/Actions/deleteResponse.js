b633420f7e511efa1ed57a13d1b0d836d9df7690
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-11-28 16:32:13 -0300

    Update deleteResponse.js

--- src/redux/Actions/deleteResponse.js
+++ src/redux/Actions/deleteResponse.js
@@ -5,7 +5,8 @@ export const DELETE_RES = "DELETE_RES";
 export function deleteRes(id) {
   return async function (dispatch) {
      //const result = await axios.delete(`http://localhost:3001/response/${id}`);
-     const result = await axios.delete(`https://formserver-t5jb.onrender.com/response/${id}`);
+     //const result = await axios.delete(`https://formserver-t5jb.onrender.com/response/${id}`);
+    const result = await axios.delete(`https://easyform-server-production.up.railway.app/response/${id}`);
     return dispatch({
       type: DELETE_RES,
       payload: result.data,
