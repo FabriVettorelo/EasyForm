307e943cbb2ddd24e49a29802053340775c518a8
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-11-28 17:51:38 -0300

    Update getForm.js

--- src/redux/Actions/getForm.js
+++ src/redux/Actions/getForm.js
@@ -4,7 +4,9 @@ export const GET_FORM = "GET_FORM";
 export const getForm =  () => {
   return async function (dispatch) {
     //const result = await axios.get("http://localhost:3001/forms");
-    const result = await axios.get("https://formserver-t5jb.onrender.com/forms")
+    //const result = await axios.get("https://formserver-t5jb.onrender.com/forms")
+    const result = await axios.get("https://easyform-server-production.up.railway.app/forms")
+    
     return dispatch({
       type: GET_FORM,
       payload: result.data,
